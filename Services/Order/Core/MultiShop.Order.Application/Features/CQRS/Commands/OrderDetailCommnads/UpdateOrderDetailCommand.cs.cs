cf1f5e5359a5f3536be309a93ef6d35e18e4fc86
mustafa <musertac@keove.com>
2024-10-23 11:00:41 +0300

    save cqrs design pattern

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Commands/OrderDetailCommnads/UpdateOrderDetailCommand.cs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommnads
+{
+    public class UpdateOrderDetailCommand
+    {
+        public int Id { get; set; }
+
+        public UpdateOrderDetailCommand(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AdressHandlers/RemoveAdressCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AdressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AdressHandlers
+{
+    public class RemoveAdressCommandHandler
+    {
+        private readonly IRepository<Address>_respository;
+
+        public RemoveAdressCommandHandler(IRepository<Address> respository)
+        {
+            _respository = respository;
+        }
+        public async Task Handle(RemoveAddressCommand command)
+        {
+            var value =await _respository.GetByIdAsync(command.Id);
+            await _respository.DeleteAsync(value);
+
+        }
+    }
+}
